a10030783c0cf7309cd6967412a4cd108c0a81a0
beepobb <yeejiazhen23@gmail.com>
2024-07-05 03:52:08 +0800

    Edit dependencies

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,20 @@
 import express, { json } from 'express';
 
+// Routes imports
+import accountRouter from '../routes/account.js'
+import bookingRouter from '../routes/booking.js'
+
 const app = express();
 
-app.use(json())
+app.use(json());
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', async (req, res) => {
     res.json({ status: true, message: "Our node.js app works" })
 });
+
+app.use('/manage-account', accountRouter);
+app.use('/manage-booking', bookingRouter);
 
 app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
