1032eec797bc90c69aa50afcedf44be85deefe11
Rylee <rjhlee123@gmail.com>
2024-07-23 19:31:57 +0800

    Server: searchHotel controller complete (further filtering by ratings, amenities, etc should be done via front-end)

--- server/src/controllers/searchHotelController.js
+++ server/src/controllers/searchHotelController.js
@@ -7,25 +7,26 @@ import { Room, fetchRoomPrices } from "../models/room.js";
 // Arguments in POST request body can be taken from req.body (e.g. req.body.name)
 // Unpack like this: const { priceRange, rating } = req.query; (names on left must match actual keys)
 
-// TODO: handle filtering
-
 async function searchHotelByDestination(req, res, next) {
 
     const id  = req.params.id; // Destination ID
     const {checkin, checkout, lang, currency, guests} = req.body;
     
     // Get list of hotels (and prices) from given destination
-    const [hotelsMap, hotelPricesMap] = await Promise.all([getCacheHotelsByDestination(id), 
+    const [hotelsMap, hotelPricesMap] = await Promise.all([cacheFetchHotelByDestination(id), 
         fetchHotelPricesByDestination(id, checkin, checkout, lang, currency, guests)]); // concurrent API fetch calls
 
-    // Further filter list of hotels accordingly
-    const filters = req.body.filters; // filters object (null means no filters)
-    const result = placeholderFilterHotels({ // TODO: SWITCH AWAY FROM PLACEHOLDER
-        hotelsMap: hotelsMap,
-        hotelPricesMap: hotelPricesMap
-    }, filters);
+    const result = []; // array
+    for (const hotelPrice of Array.from(hotelPricesMap.values())) {
+        result.push({
+            hotel: hotelsMap.get(hotelPrice.id),
+            price: hotelPrice.price
+        })
+    }
+    // Note: hotelPrice object has distance attribute which hotel object does not have for some reason
+    // If this is useful, will need to extract from hotelPrice
 
-    res.set('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.json(result);
 }
 
@@ -34,7 +35,7 @@ async function searchHotelById(req, res, next) {
     const id = req.params.id; // Hotel ID (not destination id!)
     const {destination_id, checkin, checkout, lang, currency, guests} = req.body;
 
-    const [hotel, roomList] = await Promise.all([fetchHotel(id), 
+    const [hotel, roomList] = await Promise.all([cacheFetchHotelById(id), 
         fetchRoomPrices(id, destination_id, checkin, checkout, lang, currency, guests)]);
 
     const result = {
@@ -42,13 +43,13 @@ async function searchHotelById(req, res, next) {
         rooms: roomList
     };
     
-    res.set('Access-Control-Allow-Origin', 'http://localhost:5000');
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.json(result);
 }
 
 // Cache System
 let destinationCache = {}; // In-memory cache for list of hotels obtained from fetchHotelsByDestination (persists as long as server is running)
-async function getCacheHotelsByDestination(id) {
+async function cacheFetchHotelByDestination(id) {
     if (!destinationCache[id]) { // If destination not in cache
         // Save to cache
         destinationCache[id] = await fetchHotelsByDestination(id);
@@ -60,46 +61,17 @@ async function getCacheHotelsByDestination(id) {
     return destinationCache[id];
 }
 
-function filterHotels(hotelInfo, filters) {
-    // hotelInfo will be an object with two key-value pairs: a map of hotels & a map of hotel prices
-    // Note: both maps likely not of the same length because the latter is obtained with further constraints (e.g. check in and check out date)
-    const {hotelsMap, hotelPricesMap} = hotelInfo;
-    
-    // We search for valid hotels amongst the HotelPrices instances since those are further constrained (we only use map of hotels to get hotel info)
-
-    // Possible filters: by ratings, guest ratings, price range, amenities (Other possible filters: categories, free_cancellation)
-    const {starRatingFloor, priceFloor, priceCeil, amenities} = filters;
-
-    // Convert the values in the hotel prices map (which will be the HotelPrice instances) into an array so that we can use filter() method
-    const filteredHotelPricesArray = Array.from(hotelPricesMap.values()).filter(hotelPrice => hotelPrice.price < priceCeil);
-    filteredHotelPricesArray.filter(hotelPrice => {
-        const hotel = hotelsMap.get(hotelPrice.id); // hotel object
-        return hotel.categories.overall.score >= ratingFloor;
-    })
-    // TODO: filter properly
-
-    const result = []; // array
-    for (const hotelPrice of filteredHotelPricesArray) {
-        result.push({
-            hotel: hotelsMap.get(hotelPrice.id),
-            price: hotelPrice.price
-        })
+let hotelCache = {}; // In-memory cache for specific hotel object
+async function cacheFetchHotelById(id) {
+    if (!hotelCache[id]) { // If specific hotel not in cache
+        // Save to cache
+        hotelCache[id] = await fetchHotel(id);
+        console.log(`Hotel ${id} cached`);
+    } else {
+        console.log(`Cache accessed: hotel ${id}`);
     }
-    return result;
-}
 
-
-// PLACEHOLDER FOR TESTING
-function placeholderFilterHotels(hotelInfo, filters) {
-    const {hotelsMap, hotelPricesMap} = hotelInfo;
-    const result = []; // array
-    for (const hotelPrice of Array.from(hotelPricesMap.values())) {
-        result.push({
-            hotel: hotelsMap.get(hotelPrice.id),
-            price: hotelPrice.price
-        })
-    }
-    return result;
+    return hotelCache[id];
 }
 
 export { searchHotelByDestination, searchHotelById };
--- server/src/routes/searchHotel.js
+++ server/src/routes/searchHotel.js
@@ -8,15 +8,4 @@ router.post('/destination/:id', searchHotelByDestination); // Search for list of
 
 router.get('/hotel/:id', searchHotelById); // Specific hotel details with room information
 
-
-
-// router.post('/submit/', async function(req, res, next) {
-//     const name = req.body.name;
-//     const code = req.body.code;
-//     await staffModel.insertOne(staffModel.Staff.newStaff(name, code));
-//     const staffs = await staffModel.all();
-//     res.set('Access-Control-Allow-Origin', 'http://localhost:5000');
-//     res.send(`${JSON.stringify(staffs)}`);
-// })
-
 export default router;
