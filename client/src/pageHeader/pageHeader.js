dd7cc6161059ec84347ac347738bd7483dd2ee00
Lightbulbq <121227917+Lightbulbq@users.noreply.github.com>
2024-08-01 04:45:12 +0800

    Update pageHeader.js

--- client/src/pageHeader/pageHeader.js
+++ client/src/pageHeader/pageHeader.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom'
 import { Link } from 'react-router-dom';
 import './pageHeader.css';
 
 function PageHeader() {
+    const navigate = useNavigate();
+
+    const handleLogoClick = () =>{
+        navigate("/");
+    }
     return (
         <header>
         <ul>
             <li className="Logo">
+                <button onClick={handleLogoClick} className="logobutton">
                 <img src={process.env.PUBLIC_URL + "/Ascenda_Blue_Logo.jpg"} alt="Error displaying logo" />
             </li>
             <li className="SignIn">
