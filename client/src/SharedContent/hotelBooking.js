c5cea3b950bbc91b15655a9df42cec1498b74b4b
Lightbulbq <121227917+Lightbulbq@users.noreply.github.com>
2024-08-01 04:47:16 +0800

    Update hotelBooking.js

--- client/src/SharedContent/hotelBooking.js
+++ client/src/SharedContent/hotelBooking.js
@@ -4,8 +4,10 @@ import './hotelBooking.css';
 function HotelBooking(){
     return(
         <div className="container">
-            <div className="description">Hotel Booking Description</div>
+            <h1 className="hotelname">Hotel Name</h1>
+            <div className="hoteldescription">Hotel Description</div>
         </div>
+        
     )
 }
 export default HotelBooking;
