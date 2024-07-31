cd174b4d81f1e3e268c740ce62dc23e1f99121e1
Jungstershark <ongjy1503@gmail.com>
2024-08-01 02:28:55 +0800

    Abstracted logic meant to execute frontend verification of the email and password

---
+++ client/src/UserAuth/validation.js
@@ -0,0 +1,10 @@
+export const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+  
+  export const validatePassword = (password) => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    return regex.test(password);
+  };
+  
