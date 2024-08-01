2c998b96d680d29d32a1a781cfe8d52a43fbe4c4
Jungstershark <ongjy1503@gmail.com>
2024-08-01 17:27:15 +0800

    Added register function to authServices

--- client/src/UserAuth/authServices/authServices.js
+++ client/src/UserAuth/authServices/authServices.js
@@ -10,17 +10,33 @@ const login = async (email, password) => {
     }
     return response.data;
   } catch (error) {
-    // Return error response
     if (error.response && error.response.data) {
       return error.response.data;
     } else {
-      throw error;  // Re-throw if no response data
+      throw error;
+    }
+  }
+};
+
+const register = async (email, password, username, hp) => {
+  try {
+    const response = await axios.post(API_URL + 'register', { email, password, username, hp });
+    if (response.data.token) {
+      localStorage.setItem('user', JSON.stringify(response.data));
+    }
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      return error.response.data;
+    } else {
+      throw error;
     }
   }
 };
 
 const authService = {
   login,
+  register,
 };
 
 export default authService;
