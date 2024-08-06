138b7aefb00ed4c42a941f8a2c9a341b020e66f3
Jungstershark <ongjy1503@gmail.com>
2024-08-06 11:38:11 +0800

    Fixed the page header currency drop down to fulfil unit test case

--- client/src/pageHeader/pageHeader.js
+++ client/src/pageHeader/pageHeader.js
@@ -1,32 +1,55 @@
-import React from 'react';
-import {useNavigate} from 'react-router-dom'
-import { Link } from 'react-router-dom';
+// /src/pageHeader/pageHeader.js
+
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import './pageHeader.css';
 
 function PageHeader() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [showCurrencyDropdown, setShowCurrencyDropdown] = useState(false);
+  const [selectedCurrency, setSelectedCurrency] = useState('SGD');
 
-    const handleLogoClick = () =>{
-        navigate("/");
-    }
-    return (
-        <header>
-        <ul>
-            <li className="Logo">
-                <button onClick={handleLogoClick} className="logobutton">
-                <img src={process.env.PUBLIC_URL + "/Ascenda_Blue_Logo.jpg"} alt="Error displaying logo" />
-                </button>
-            </li>
-            <li className="SignIn">
-                <Link to="/login">Sign in</Link>
-            </li>
-            <li className="ContactUs">
-                <a href="#contactus">Contact Us</a>
-            </li>
-            <li className="SGD"><button>SGD ▼</button></li>
-        </ul>
-        </header>
-    );
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
+  const handleCurrencyClick = () => {
+    setShowCurrencyDropdown(!showCurrencyDropdown);
+  };
+
+  const handleCurrencySelect = (currency) => {
+    setSelectedCurrency(currency);
+    setShowCurrencyDropdown(false);
+  };
+
+  return (
+    <header>
+      <ul>
+        <li className="Logo">
+          <button onClick={handleLogoClick} className="logobutton">
+            <img src={process.env.PUBLIC_URL + "/Ascenda_Blue_Logo.jpg"} alt="Error displaying logo" />
+          </button>
+        </li>
+        <li className="SignIn">
+          <Link to="/login">Sign in</Link>
+        </li>
+        <li className="ContactUs">
+          <a href="#contactus">Contact Us</a>
+        </li>
+        <li className="SGD">
+          <button onClick={handleCurrencyClick}>{selectedCurrency} ▼</button>
+          {showCurrencyDropdown && (
+            <div id="currency-dropdown">
+              <div onClick={() => handleCurrencySelect('SGD')}>SGD</div>
+              <div onClick={() => handleCurrencySelect('USD')}>USD</div>
+              <div onClick={() => handleCurrencySelect('EUR')}>EUR</div>
+              <div onClick={() => handleCurrencySelect('JPY')}>JPY</div>
+            </div>
+          )}
+        </li>
+      </ul>
+    </header>
+  );
 }
 
 export default PageHeader;
